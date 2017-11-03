b6a44352be55936feda043af9b2dbd92c22dfc7a
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-03 14:40:26 +0900

    マシン語を直書きの部分を、アセンブリ言語に書き換え。

--- helloos.s
+++ helloos.s
@@ -1,5 +1,11 @@
-# FAT12 フロッピーディスクのための記述
-	.byte	0xeb, 0x4e, 0x90
+	.file "helloos.s"
+	
+.code16
+.text
+	jmp entry		# entryから処理を開始する
+	
+	# FAT12 フロッピーディスクであることの宣言
+	.byte	0x90
 	.ascii 	"HELLOIPL"	# ブートセクタの名前(8バイト)
 	.word 	512		# 1セクタのサイズ
 	.byte 	1		# クラスタのサイズ
@@ -18,22 +24,37 @@
 	.ascii	"HELLO-OS   "	# ディスクの名前(11バイト)
 	.ascii	"FAT12   "	# フォーマットの名前(8バイト)
 	.skip	18, 0x00	# 18バイト空けておく
+	
+entry:	# プログラム本体	
+	# 各種レジスタの初期化
+	movw	$0, %ax
+	movw	%ax, %ss
+	movw	$0x7c00, %sp
+	movw	%ax, %ds
+	movw	%ax, %es
+	
+	movw	$msg, %si
+	
+putloop: # 文字表示ループ。NUL文字が来るまで、ソフトウェア割り込み命令を使って、文字を表示していく。
+	movb	(%si), %al
+	addw	$1, %si
+	cmpb	$0, %al		# NUL文字かどうか？
+	je	fin		# NUL文字だったら、終了へ
+	movb	$0x0e, %ah	# 1文字表示ファンクション
+	movw	$15, %bx	# カラーコード
+	int	$0x10		# ビデオBIOS呼び出し
+	jmp	putloop
 
-# プログラム本体	
-	.byte 	0xb8, 0x00, 0x00, 0x8e, 0xd0, 0xbc, 0x00, 0x7c, 0x8e, 0xd8, 0x8e, 0xc0, 0xbe, 0x74, 0x7c, 0x8a
-	.byte 	0x04, 0x83, 0xc6, 0x01, 0x3c, 0x00, 0x74, 0x09, 0xb4, 0x0e, 0xbb, 0x0f, 0x00, 0xcd, 0x10, 0xeb
-	.byte 	0xee, 0xf4, 0xeb, 0xfd
+fin:
+	hlt			# 何かあるまでCPUを停止させる
+	jmp	fin
 
-# メッセージ部分	
+msg:	# メッセージ部分
 	.byte	0x0a, 0x0a	# 改行を2つ
 	.ascii	"hello, world"
 	.byte 	0x0a		# 改行
-	.byte 	0
+	.byte 	0		# NUL文字
 
 	.org	0x1fe, 0x00	# 0x1f0まで、0で埋める
 	
-	.byte	0x55, 0xaa
-	.byte 	0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-	.skip 	4600, 0x00
-	.byte 	0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-	.skip 	1469432, 0x00
+	.byte	0x55, 0xaa	# ブート・シグニチャ(このマジックナンバーがないとマスターブートレコードとして有効にならない)
