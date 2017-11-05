b8c8a4267207c8d78755cd1fb8824252f821b013
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-05 11:27:02 +0900

    write_memの代わりにに、ポインタを使用する。

--- bootpack.c
+++ bootpack.c
@@ -4,9 +4,11 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
   int i;
+  char *p;
 
   for (i = 0xa0000; i <= 0xaffff; i++) {
-    write_mem8(i, i & 0xf);
+    p = i;
+    *p = i & 0xf;
   }
 
   for (;;) {
