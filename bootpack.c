9a89adcb21b34a5bf86df5b3336aed0ba5229563
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-05 11:17:10 +0900

    画面を白くするように変更。

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,15 @@
 void io_halt();
+void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
- fin:
-  io_halt();
-  goto fin;
+  int i;
+
+  for (i = 0xa0000; i <= 0xaffff; i++) {
+    write_mem8(i, 15);
+  }
+
+  for (;;) {
+    io_halt();
+  }
 }
--- naskfunc.s
+++ naskfunc.s
@@ -1,7 +1,15 @@
 .code32
 	.global	io_halt
+	.global write_mem8
 
 .text
 io_halt:	# void io_halt(void) ;
 	hlt
 	ret
+
+write_mem8:	# void write_mem8(int addr, int data) ;
+	movl	4(%esp), %ecx	# %esp+4 にaddrが入っているので、ecxに
+	movb	8(%esp), %al	# %esp+8 にdataが入っているので、alに
+	movb	%al, (%ecx)
+	ret
+	
