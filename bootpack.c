e518c35714b0cf0580b89bf2bd214c418d990901
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-06 10:48:19 +0900

    Aの文字を表示。

--- bootpack.c
+++ bootpack.c
@@ -7,6 +7,7 @@ void io_store_eflags(int eflags);
 void init_pallete(void);
 void set_pallete(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void putfont8(unsigned char *vram, int xsize, int x, int y, char c, char *font);
 
 void init_screen(unsigned char *vram, int xsize, int ysize);
 
@@ -37,8 +38,14 @@ void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
   
+  static char font_A[16] = {
+    0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+    0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+  };
+  
   init_pallete();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+  putfont8(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFFFF, font_A);
   
   for (;;) {
     io_hlt();
@@ -117,4 +124,26 @@ void init_screen(unsigned char *vram, int xsize, int ysize)
   boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
   boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
   boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+  return;
+}
+
+void putfont8(unsigned char *vram, int xsize, int x, int y, char c, char *font)
+{
+  int dummy = 0;
+  int i;
+  unsigned char *p;
+  char d;
+  for (i = 0; i < 16; i++) {
+    p = vram + (y + i) * xsize + (x + dummy);
+    d = font[i];
+    if ((d & 0x80) != 0) { p[0] = c; }
+    if ((d & 0x40) != 0) { p[1] = c; }
+    if ((d & 0x20) != 0) { p[2] = c; }
+    if ((d & 0x10) != 0) { p[3] = c; }
+    if ((d & 0x08) != 0) { p[4] = c; }
+    if ((d & 0x04) != 0) { p[5] = c; }
+    if ((d & 0x02) != 0) { p[6] = c; }
+    if ((d & 0x01) != 0) { p[7] = c; }
+  }
+  return;
 }
