aa33d3d1a960f6ba448c77f5cc2c27b60f64ddf9
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-07 18:37:46 +0900

    マウスを有効化する。

--- bootpack.c
+++ bootpack.c
@@ -3,7 +3,24 @@
 #include "lib/aolib.h"
 #include "bootpack.h"
 
+#define PORT_KEYDAT	0x0060
+#define PORT_KEYSTA	0x0064
+#define PORT_KEYCMD	0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE	0x60
+#define KBC_MODE	0x47
+
+void wait_KBC_sendready(void);
+void init_keyboard(void);
+
+#define KEYCMD_SENDTO_MOUSE	0xd4
+#define MOUSE_ENABLE	0xf4
+
+void enable_mouse(void);
+
+
 extern struct FIFO8 keyfifo;
+
 
 void HariMain(void)
 {
@@ -22,6 +39,11 @@ void HariMain(void)
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(mcursor, COL8_008484);
 
+  fifo8_init(&keyfifo, 32, keybuf);
+  io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+  io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+  init_keyboard();
+  
   /* マウス・カーソルを画面中心になるように計算し描画 */
   mx = (binfo->scrnx - 16) / 2;
   my = (binfo->scrny - 28 - 16) / 2;
@@ -30,10 +52,7 @@ void HariMain(void)
   sprintk(msg, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, msg);
 
-  fifo8_init(&keyfifo, 32, keybuf);
-  
-  io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
-  io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+  enable_mouse();
   
   for (;;) {
     io_cli();
@@ -48,4 +67,35 @@ void HariMain(void)
       putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
     }
   }
+}
+
+void wait_KBC_sendready(void)
+{
+  /* キーボード・コントローラがデータ送信可能になるまで待つ */
+  for (;;) {
+    if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+      break;
+    }
+  }
+  return;
+}
+
+void init_keyboard(void)
+{
+  /* キーボード・コントローラの初期化 */
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, KBC_MODE);
+  return;
+}
+
+void enable_mouse(void)
+{
+  /* マウス有効 */
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, MOUSE_ENABLE);
+  return;
 }
