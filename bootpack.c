a754712731c05e032d30b724c405cfcb93cc8adf
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-07 14:06:36 +0900

    PIC(Programable Interrupt Controller)の初期化を実装。

--- bootpack.c
+++ bootpack.c
@@ -8,18 +8,23 @@ void HariMain(void)
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char mcursor[16 * 16];
   char msg[256];
-  int mx = 152;
-  int my = 78;
+  int mx;
+  int my;
 
   init_gdtidt();
+  init_pic();
+  
   init_pallete();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(mcursor, COL8_008484);
+
+  /* マウス・カーソルを画面中心になるように計算し描画 */
+  mx = (binfo->scrnx - 16) / 2;
+  my = (binfo->scrny - 28 - 16) / 2;
+  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
   
   sprintk(msg, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, msg);
-  
-  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
   
   for (;;) {
     io_hlt();
---
+++ int.c
@@ -0,0 +1,22 @@
+#include "bootpack.h"
+
+void init_pic(void)
+{
+  io_out8(PIC0_IMR, 0xff);	/* 全ての割り込みを受け付けない */
+  io_out8(PIC1_IMR, 0xff);	/* 全ての割り込みを受け付けない */
+  
+  io_out8(PIC0_ICW1, 0x11  );	/* エッジ・トリガー・モード */
+  io_out8(PIC0_ICW2, 0x20  );	/* IRQ0-7は、INT20-27で受け取る */
+  io_out8(PIC0_ICW3, 1 << 2);	/* PIC1は、IRQ2にて接続 */
+  io_out8(PIC0_ICW4, 0x01  );	/* ノン・バッファ・モード */
+  
+  io_out8(PIC1_ICW1, 0x11  );	/* エッジ・トリガー・モード */
+  io_out8(PIC1_ICW2, 0x28  );	/* IRQ8-15は、INT28-2fで受け取る */
+  io_out8(PIC1_ICW3, 1 << 2);	/* PIC1は、IRQ2にて接続 */
+  io_out8(PIC1_ICW4, 0x01  );	/* ノン・バッファ・モード */
+  
+  io_out8(PIC0_IMR, 0xfb);	/* 11111011 PIC1以外は禁止 */
+  io_out8(PIC1_IMR, 0xff);	/* 11111111 全ての割り込みを受け付けない */
+
+  return;
+}
