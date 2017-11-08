8d293d0b40a78b54c666cce66d82ea4f2ed479df
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-08 15:28:56 +0900

    メモリ管理をする。

--- bootpack.c
+++ bootpack.c
@@ -6,6 +6,127 @@
 #define EFLAGS_AC_BIT		0x00040000
 #define CR0_CACHE_DISABLE	0x60000000
 
+#define MEMMAN_FREES		4090 /* これで約32KB */
+
+struct FREEINFO {		/* 空き情報 */
+  unsigned int addr, size;
+};
+
+struct MEMMAN {			/* メモリ管理 */
+  int frees, maxfrees, lostsize, losts;
+  struct FREEINFO free[MEMMAN_FREES];
+};
+
+void memman_init(struct MEMMAN *man)
+{
+  man->frees = 0;	/* 空きの個数 */
+  man->maxfrees = 0;	/* 状況観察用:freeの最大値 */
+  man->lostsize = 0;	/* 解放に失敗した合計サイズ */
+  man->losts = 0;	/* 解放に失敗した回数 */
+  return;
+};
+
+/* 空きサイズの合計を報告 */
+unsigned int memman_total(struct MEMMAN *man)
+{
+  unsigned int i, t = 0;
+  for (i = 0; i < man->frees; i++) {
+    t += man->free[i].size;
+  }
+  return t;
+}
+
+/* メモリを確保 */
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+  unsigned int i, a;
+  for (i = 0; i < man->frees; i++) {
+    if (man->free[i].size >= size) {
+      /* 十分な広さの空きを発見 */
+      a = man->free[i].addr;
+      man->free[i].addr += size;
+      man->free[i].size -= size;
+      if (man->free[i].size == 0) {
+	/* free[i]がなくなったので前に詰める */
+	man->frees--;
+	for (; i < man->frees; i++) {
+	  man->free[i] = man->free[i + 1];
+	}
+      }
+      return a;
+    }
+  }
+  return 0;			/* 空きがない */
+}
+
+/* メモリを解放 */
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+  int i, j;
+  /* まとめやすさを考えると、free[]がaddr順に並んでいる方がいい */
+  /* まず、どこに入れるべきかを決める */
+  for (i = 0; i < man->frees; i++) {
+    if (man->free[i].addr > addr) {
+      break;
+    }
+  }
+  /* free[i - 1].addr < addr < free[i].addrになる */
+  
+  if (i > 0) {
+    /* 前に空き領域ある */
+    
+    if (man->free[i - 1].addr + man->free[i -1].size == addr) {
+      /* 前の空き領域にまとめられる */
+      
+      man->free[i - 1].size += size;
+      if (i < man->frees) {
+	/* 後ろにも空き領域がある */
+	if (addr + size == man->free[i].addr) {
+	  /* 後ろもまとめられる */
+	  man->free[i - 1].size += man->free[i].size;
+	  /* man->free[i]は不要。残りを前につめる */
+	  man->frees--;
+	  for (; i < man->frees; i++) {
+	    man->free[i] = man->free[i + 1];
+	  }
+	}
+      }
+      return 0;			/* 成功終了 */
+    }
+  }
+
+  /* 前とはまとめられなかった */
+  if (i < man->frees) {
+    /* 後ろに空き領域がある */
+    if (addr + size == man->free[i].addr) {
+      /* 後ろとまとめられる */
+      man->free[i].addr = addr;
+      man->free[i].size += size;
+      return 0;	    /* 成功終了 */
+    }
+  }
+
+  /* 前にも後ろにもまとめられなかった */
+  if (man->frees < MEMMAN_FREES) {
+    /* free[i]から後ろを、 後ろへずらして、free[i]を使えるようにする*/
+    for (j = man->frees; j > i; j--) {
+      man->free[j] = man->free[j - 1];
+    }
+    man->frees++;
+    if (man->maxfrees < man->frees) {
+      man->maxfrees = man->frees; /* 最大値を更新 */
+    }
+    man->free[i].addr = addr;
+    man->free[i].size = size;
+    return 0;			/* 成功終了 */
+  }
+
+  /* 後ろにずらせなかった */
+  man->losts++;
+  man->lostsize += size;
+  return -1;			/* 失敗終了 */
+}
+
 int load_cr0();
 void store_cr0(int cr0);
 unsigned int memtest(unsigned int start, unsigned int end);
@@ -13,17 +134,20 @@ unsigned int memtest_sub(unsigned int start, unsigned int end);
 
 extern struct FIFO8 keyfifo, mousefifo;
 
+#define MEMMAN_ADDR	0x003c0000
 
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char mcursor[16 * 16];
   char msg[256], s[16], keybuf[32], mousebuf[128];
-  int mx;
-  int my;
+  int mx, my;
   int i;
-
+  
   struct MOUSE_DEC mdec;
+
+  unsigned int memtotal;
+  struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
   init_gdtidt();
   init_pic();
@@ -49,8 +173,12 @@ void HariMain(void)
 
   enable_mouse(&mdec);
 
-  i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-  sprintk(msg, "memory %dMB", i);
+  memtotal = memtest(0x00400000, 0xbfffffff);
+  memman_init(memman);
+  memman_free(memman, 0x00001000, 0x0009e000);
+  memman_free(memman, 0x00400000, memtotal - 0x00400000);
+  sprintk(msg, "memory %dMB   free : %dKB",
+	  memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, msg);
   
   for (;;) {
