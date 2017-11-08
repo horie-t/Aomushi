a3868f0a97b390710606509f801cecada719e980
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-08 18:03:50 +0900

    4KB単位でメモリを確保する。コード整理。

--- bootpack.h
+++ bootpack.h
@@ -26,6 +26,9 @@ void io_store_eflags(int eflags);
 void load_gdtr(int limit, int addr);
 void load_idtr(int limit, int addr);
 
+int load_cr0();
+void store_cr0(int cr0);
+
 void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
@@ -71,6 +74,28 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_CODE32_ER	0x409a
 
 #define AR_INTGATE32	0x008e
+
+/* memory.c */
+#define MEMMAN_FREES		4090 /* これで約32KB */
+#define MEMMAN_ADDR	0x003c0000
+
+struct FREEINFO {		/* 空き情報 */
+  unsigned int addr, size;
+};
+
+struct MEMMAN {			/* メモリ管理 */
+  int frees, maxfrees, lostsize, losts;
+  struct FREEINFO free[MEMMAN_FREES];
+};
+
+unsigned int memtest(unsigned int start, unsigned int end);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 /* int.c */
 struct KEYBUF {
