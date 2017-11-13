f630635f9b3af01e3dcf3e88b2600722be4617e1
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-13 09:56:41 +0900

    マウスでウィンドウを移動できるようにする

--- bootpack.c
+++ bootpack.c
@@ -229,6 +229,11 @@ void HariMain(void)
 	  sprintk(msg, "(%3d, %3d)", mx, my);
 	  putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF, COL8_008484, msg, 10);
 	  sheet_slide(sht_mouse, mx, my); /* sheet_refreshを含む */
+
+	  if (mdec.btn & 0x01 != 0) {
+	    /* 左ボタンを押していたら、sht_winを動かす。 */
+	    sheet_slide(sht_win, mx - 80, my - 8);
+	  }
 	}
       } else if (i == 10) {
 	putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
