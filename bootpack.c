31453c19d1b5324bead3c7d09965c973605de887
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-13 11:47:22 +0900

    タスクBでカウンタを実行。

--- bootpack.c
+++ bootpack.c
@@ -78,15 +78,24 @@ void task_b_main(void)
   struct TIMER *timer_ts;
   int i, fifobuf[128];
 
+  int count = 0;
+  char s[11];
+  struct SHEET *sht_back;
+
   fifo32_init(&fifo, 128, fifobuf);
   timer_ts = timer_alloc();
   timer_init(timer_ts, &fifo, 1);
   timer_settime(timer_ts, 2);
+  sht_back = (struct SHEET *) *((int *) 0x0fec);
 
   for (;;) {
+    count++;
+    sprintk(s, "%10d", count);
+    putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
+    
     io_cli();
     if (fifo32_status(&fifo) == 0) {
-      io_stihlt();
+      io_sti();
     } else {
       i = fifo32_get(&fifo);
       io_sti();
@@ -169,6 +178,7 @@ void HariMain(void)
   shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
   
   sht_back = sheet_alloc(shtctl);
+  
   sht_mouse = sheet_alloc(shtctl);
   sht_win = sheet_alloc(shtctl);
   
@@ -228,6 +238,7 @@ void HariMain(void)
   tss_b.ds = 1 * 8;
   tss_b.fs = 1 * 8;
   tss_b.gs = 1 * 8;
+  *((int *) 0x0fec) = (int) sht_back;
   
   for (;;) {
     io_cli();
