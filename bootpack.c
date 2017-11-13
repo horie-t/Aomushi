18215be4b0c5adea6c8d44e526ced228a5f5763d
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-13 09:52:32 +0900

    テキスト入力エリアを実装

--- bootpack.c
+++ bootpack.c
@@ -57,6 +57,21 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
   return;
 }
 
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c)
+{
+  int x1 = x0 + sx, y1 = y0 + sy;
+  boxfill8(sht->buf, sht->bxsize, COL8_848484, x0 - 2, y0 - 3, x1 + 1, y0 - 3);
+  boxfill8(sht->buf, sht->bxsize, COL8_848484, x0 - 3, y0 - 3, x0 - 3, y1 + 1);
+  boxfill8(sht->buf, sht->bxsize, COL8_FFFFFF, x0 - 3, y1 + 2, x1 + 1, y1 + 2);
+  boxfill8(sht->buf, sht->bxsize, COL8_FFFFFF, x1 + 2, y0 - 3, x1 + 2, y1 + 2);
+  boxfill8(sht->buf, sht->bxsize, COL8_000000, x0 - 1, y0 - 2, x1 + 0, y0 - 2);
+  boxfill8(sht->buf, sht->bxsize, COL8_000000, x0 - 2, y0 - 2, x0 - 2, y1 + 0);
+  boxfill8(sht->buf, sht->bxsize, COL8_C6C6C6, x0 - 2, y1 + 1, x1 + 0, y1 + 1);
+  boxfill8(sht->buf, sht->bxsize, COL8_C6C6C6, x1 + 1, y0 - 2, x1 + 1, y1 + 1);
+  boxfill8(sht->buf, sht->bxsize,           c, x0 - 1, y0 - 1, x1 + 0, y1 + 0);
+  return;
+}
+
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
@@ -66,6 +81,7 @@ void HariMain(void)
   struct TIMER *timer, *timer2, *timer3;
   int mx, my;
   int i;
+  int cursor_x, cursor_c;
 
   struct MOUSE_DEC mdec;
 
@@ -129,6 +145,10 @@ void HariMain(void)
   init_screen8(buf_back, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(buf_mouse, 99); /* 背景色は99 */
   make_window8(buf_win, 160, 52, "window");
+
+  make_textbox8(sht_win, 8, 28, 144, 16, COL8_FFFFFF);
+  cursor_x = 8;
+  cursor_c = COL8_FFFFFF;
   
   sheet_slide(sht_back, 0, 0);
   mx = (binfo->scrnx - 16) / 2;	/* 画面中央になるように座標計算 */
@@ -161,12 +181,21 @@ void HariMain(void)
 	putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
 
 	if (i < 256 + 0x54) {
-	  if (keytable[i - 256] != 0) {
+	  if (keytable[i - 256] != 0 && cursor_x < 144) {
 	    s[0] = keytable[i - 256];
 	    s[1] = 0;
-	    putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 1);
+	    putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
+	    cursor_x += 8;
 	  }
 	}
+	if (i == 256 + 0x0e && cursor_x > 8) { /* バックスペース */
+	  /* カーソルをスペースで消してから、カーソルを一つ戻す */
+	  putfonts8_asc_sht(sht_win,  cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
+	  cursor_x -= 8;
+	}
+	/* カーソルの再表示 */
+	boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+	sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
       } else if (512 <= i && i <= 767) {
 	if (mouse_decode(&mdec, i - 512) != 0) {
 	  /* データが3バイト揃ったので表示 */
@@ -205,16 +234,17 @@ void HariMain(void)
 	putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
       } else if (i == 3) {
 	putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-      } else if (i == 1) {
-	timer_init(timer3, &fifo, 0); /* 次は0を */
-	boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+      } else if (i <= 1) {
+	if (i != 0) {
+	  timer_init(timer3, &fifo, 0); /* 次は0を */
+	  cursor_c = COL8_000000;
+	} else {
+	  timer_init(timer3, &fifo, 1); /* 次は0を */
+	  cursor_c = COL8_FFFFFF;
+	}
 	timer_settime(timer3, 50);
-	sheet_refresh(sht_back, 8, 96, 16, 112);
-      } else if (i == 0) {
-	timer_init(timer3, &fifo, 1); /* 次は1を */
-	boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
-	timer_settime(timer3, 50);
-	sheet_refresh(sht_back, 8, 96, 16, 112);
+	boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+	sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
       }
     }
   }
