b93d69cbea556ef8b4f68dc7952572a98db4866d
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-15 22:32:08 +0900

    アプリケーション名を自由にする

--- console.c
+++ console.c
@@ -170,13 +170,13 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
     cmd_dir(cons);
   } else if (strncmp(cmdline, "type ", 5) == 0) {
     cmd_type(cons, fat, cmdline);
-  } else if (strcmp(cmdline, "hlt") == 0) {
-    cmd_hlt(cons, fat);
   } else if (cmdline[0] != 0) {
-    /* コマンドではなく、さらに空行でもない */
-    putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
-    cons_newline(cons);
-    cons_newline(cons);
+    if (cmd_app(cons, fat, cmdline) == 0) {
+      /* コマンドではなく、さらに空行でもない */
+      putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
+      cons_newline(cons);
+      cons_newline(cons);
+    }
   }
 
   return;
@@ -291,3 +291,47 @@ void cmd_hlt(struct CONSOLE *cons, int *fat)
 
   return;
 }
+
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
+{
+  struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+  struct FILEINFO *finfo;
+  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+  char name[18], *p;
+  int i;
+
+  /* コマンド・ラインからファイル名を生成 */
+  for (i = 0; i < 13; i++) {
+    if (cmdline[i] <= ' ') {
+      break;
+    }
+    name[i] = cmdline[i];
+  }
+  name[i] = 0;			/* とりあえず、ファイル名の後ろを0にする */
+
+  /* ファイルを探す */
+  finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+  if (finfo == 0 && name[i - 1] != '.') {
+    name[i    ] = '.';
+    name[i + 1] = 'H';
+    name[i + 2] = 'R';
+    name[i + 3] = 'B';
+    name[i + 4] = 0;
+    finfo = file_search(name, (struct FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+  }
+    
+  if (finfo != 0) {
+    /* ファイルが見つかった場合 */
+    p = (char *)memman_alloc_4k(memman, finfo->size);
+    file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
+    set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER);
+    farcall(0, 1003 * 8);
+    memman_free_4k(memman, (int)p, finfo->size);
+    cons_newline(cons);
+    return 1;
+  }
+
+  /* ファイルが見つからなかった場合 */
+  return 0;			
+}
+
