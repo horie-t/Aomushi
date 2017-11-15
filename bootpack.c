07b195f29196b4caa2b4384261172369b2cacef1
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-15 10:15:59 +0900

    FAT対応

--- bootpack.c
+++ bootpack.c
@@ -111,6 +111,41 @@ int cons_newline(int cursor_y, struct SHEET *sheet)
   return cursor_y;
 }
 
+/* ディスク・イメージ内の圧縮を解く */
+void file_read_fat(int *fat, unsigned char *img)
+{
+  int i, j = 0;
+
+  for (i = 0; i < 2880; i += 2) {
+    fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;
+    fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;
+    j += 3;
+  }
+  return;
+}
+
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img)
+{
+  int i;
+
+  for (;;) {
+    if (size <= 512) {
+      for (i = 0; i < size; i++) {
+	buf[i] = img[clustno * 512 + i];
+      }
+      break;
+    }
+    for (i = 0; i < 512; i++) {
+      buf[i] = img[clustno * 512 + i];
+    }
+
+    size -= 512;
+    buf += 512;
+    clustno = fat[clustno];
+  }
+  return;
+}
+
 void console_task(struct SHEET *sheet, unsigned int memtotal)
 {
   struct TIMER *timer;
@@ -124,12 +159,16 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
   struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
   
   struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG + 0x002600);
+  int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
 
   fifo32_init(&task->fifo, 128, fifobuf, task);
 
   timer = timer_alloc();
   timer_init(timer, &task->fifo, 1);
   timer_settime(timer, 50);
+
+  /* FAT */
+  file_read_fat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
 
   /* プロンプト表示 */
   putfonts8_asc_sht(sheet, 8, 28, COL8_FFFFFF, COL8_000000, ">", 1);
@@ -256,12 +295,12 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 	    }
 	    if (x < 224 && finfo[x].name[0] != 0x00) {
 	      /* ファイルが見つかった場合 */
-	      y = finfo[x].size;
-	      p = (char *)(finfo[x].clustno * 512 + 0x003e00 + ADR_DISKIMG);
+	      p = (char *)memman_alloc_4k(memman, finfo[x].size);
+	      file_loadfile(finfo[x].clustno, finfo[x].size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
 	      cursor_x = 8;
-	      for (x = 0; x < y; x++) {
+	      for (y = 0; y < finfo[x].size; y++) {
 		/* 一文字ずつ出力 */
-		s[0] = p[x];
+		s[0] = p[y];
 		s[1] = 0;
 		if (s[0] == 0x09) { /* タブ */
 		  for (;;) {
@@ -289,6 +328,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 		  }
 		}
 	      }
+	      memman_free_4k(memman, (int) p, finfo[x].size);
 	    } else {
 	      /* ファイルが見つからなかった場合 */
 	      putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
