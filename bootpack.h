b95af8f32a0ef905a5e9fe620b8af0466440cd8d
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-16 09:13:17 +0900

    コンソールに文字列を表示する関数を追加

--- bootpack.h
+++ bootpack.h
@@ -315,9 +315,12 @@ struct CONSOLE {
 void console_task(struct SHEET *sheet, unsigned int memtotal);
 void cons_newline(struct CONSOLE *cons);
 void cons_putchar(struct CONSOLE *cons, int chr, char move);
+void cons_putstr0(struct CONSOLE *cons, char *s);
+void cons_putstr1(struct CONSOLE *cons, char *s, int l);
 void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal);
 void cmd_mem(struct CONSOLE *cons, unsigned int memtotal);
 void cmd_cls(struct CONSOLE *cons);
 void cmd_dir(struct CONSOLE *cons);
 void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
 void cmd_hlt(struct CONSOLE *cons, int *fat);
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
--- console.c
+++ console.c
@@ -160,6 +160,23 @@ void cons_putchar(struct CONSOLE *cons, int chr, char move)
   return;
 }
 
+void cons_putstr0(struct CONSOLE *cons, char *s)
+{
+  for (; *s != 0; s++) {
+    cons_putchar(cons, *s, 1);
+  }
+  return;
+}
+
+void cons_putstr1(struct CONSOLE *cons, char *s, int l)
+{
+  int i;
+  for (i = 0; i < l; i++) {
+    cons_putchar(cons, s[i], 1);
+  }
+  return;
+}
+
 void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal)
 {
   if (strcmp(cmdline, "mem") == 0) {
@@ -173,9 +190,7 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
   } else if (cmdline[0] != 0) {
     if (cmd_app(cons, fat, cmdline) == 0) {
       /* コマンドではなく、さらに空行でもない */
-      putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
-      cons_newline(cons);
-      cons_newline(cons);
+      cons_putstr0(cons, "Bad command.\n\n");
     }
   }
 
@@ -185,15 +200,10 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
 void cmd_mem(struct CONSOLE *cons, unsigned int memtotal)
 {
   struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-  char s[30];
-  
-  sprintf(s, "total   %dMB", memtotal / (1024 * 1024));
-  putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
-  cons_newline(cons);
-  sprintf(s, "free %dKB", memman_total(memman) / 1024);
-  putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
-  cons_newline(cons);
-  cons_newline(cons);
+  char s[60];
+
+  sprintf(s, "total   %dMB\nfree %dKB\n\n", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+  cons_putstr0(cons, s);
 
   return;
 }
@@ -226,15 +236,14 @@ void cmd_dir(struct CONSOLE *cons)
     }
     if (finfo[i].name[0] != 0xe5) {
       if ((finfo[i].type & 0x18) == 0) {
-	sprintf(s, "filename.ext   %7d", finfo[i].size);
+	sprintf(s, "filename.ext   %7d\n", finfo[i].size);
 	for (j = 0; j < 8; j++) {
 	  s[j] = finfo[i].name[j];
 	}
 	s[ 9] = finfo[i].ext[0];
 	s[10] = finfo[i].ext[1];
 	s[11] = finfo[i].ext[2];
-	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
-	cons_newline(cons);
+	cons_putstr0(cons, s);
       }
     }
   }
@@ -254,14 +263,11 @@ void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline)
     /* ファイルが見つかった場合 */
     p = (char *)memman_alloc_4k(memman, finfo->size);
     file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
-    for (i = 0; i < finfo->size; i++) {
-      cons_putchar(cons, p[i], 1);
-    }
+    cons_putstr1(cons, p, finfo->size);
     memman_free_4k(memman, (int) p, finfo->size);
   } else {
     /* ファイルが見つからなかった場合 */
-    putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
-    cons_newline(cons);
+    cons_putstr0(cons,  "File not found.\n");
   }
   cons_newline(cons);
 
