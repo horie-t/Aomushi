1b906b86e71c82edf404674ad7f3d2eaced7ac18
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-17 13:08:29 +0900

    アプリのコード・セグメントのアドレスを調整。

--- console.c
+++ console.c
@@ -281,6 +281,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
   struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
   char name[18], *p, *q;
   struct TASK *task = task_now();
+  int segsiz, datsiz, esp, dathrb;
   int i;
 
   /* コマンド・ラインからファイル名を生成 */
@@ -306,18 +307,25 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
   if (finfo != 0) {
     /* ファイルが見つかった場合 */
     p = (char *)memman_alloc_4k(memman, finfo->size);
-    q = (char *)memman_alloc_4k(memman, 64 * 1024);
-    *((int *)0x0fe8) = (int)p;
     file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
-    set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
-    set_segmdesc(gdt + 1004, 64 * 1024 - 1, (int)q, AR_DATA32_RW + 0x60);
-    if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) {
-      start_app(0x1b, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+    if (finfo->size >= 36 && strncmp(p + 4, "Hari", 4) == 0 && *p == 0x00) {
+      segsiz = *((int *) (p + 0x0000));
+      esp    = *((int *) (p + 0x000c));
+      datsiz = *((int *) (p + 0x0010));
+      dathrb = *((int *) (p + 0x0014));
+      q = (char *)memman_alloc_4k(memman, segsiz);
+      *((int *)0x0fe8) = (int)q;
+      set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+      set_segmdesc(gdt + 1004, segsiz - 1,      (int)q, AR_DATA32_RW + 0x60);
+      for (i = 0; i < datsiz; i++) {
+	q[esp + i] = p[dathrb + i];
+      }
+      start_app(0x1b, 1003 * 8, esp, 1004 * 8, &(task->tss.esp0));
+      memman_free_4k(memman, (int)q, segsiz);
     } else {
-      start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+      cons_putstr0(cons, ".hrb file format error.\n");
     }      
     memman_free_4k(memman, (int)p, finfo->size);
-    memman_free_4k(memman, (int)q, 64 * 1024);
     cons_newline(cons);
     return 1;
   }
@@ -331,11 +339,14 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
   int cs_base = *((int *)0x0fe8);
   struct TASK *task = task_now();
   struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+  char s[128];
   
   if (edx == 1) {
     cons_putchar(cons, eax & 0xff, 1);
   } else if (edx == 2) {
     cons_putstr0(cons, (char *)ebx + cs_base);
+    /* sprintf(s, "EBX: 0x%08X. CS_BASE: 0x%08X\n", ebx, cs_base); */
+    /* cons_putstr0(cons, s); */
   } else if (edx == 3) {
     cons_putstr1(cons, (char *)ebx + cs_base, ecx);
   } else if (edx == 4) {
