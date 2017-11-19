2901a1eb9a50efd4b179a11ad270ff7ef2606706
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-19 18:09:50 +0900

    LDT(Local Descriptor Table)を使用するように変更

--- bootpack.h
+++ bootpack.h
@@ -99,6 +99,8 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_DATA32_RW	0x4092
 #define AR_CODE32_ER	0x409a
 
+#define AR_LDT		0x0082
+
 #define AR_TSS32	0x0089
 #define AR_INTGATE32	0x008e
 
@@ -194,6 +196,7 @@ struct TASK {
   int level, priority;
   struct FIFO32 fifo;
   struct TSS32 tss;
+  struct SEGMENT_DESCRIPTOR ldt[2];
   struct CONSOLE *cons;
   int ds_base, cons_stack;
 };
--- console.c
+++ console.c
@@ -396,14 +396,14 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
       dathrb = *((int *) (p + 0x0014));
       q = (char *)memman_alloc_4k(memman, segsiz);
       task->ds_base = (int)q;
-      set_segmdesc(gdt + task->sel / 8 + 1000, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
-      set_segmdesc(gdt + task->sel / 8 + 2000, segsiz - 1,      (int)q, AR_DATA32_RW + 0x60);
+      set_segmdesc(task->ldt + 0, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+      set_segmdesc(task->ldt + 1, segsiz - 1,      (int)q, AR_DATA32_RW + 0x60);
       for (i = 0; i < datsiz; i++) {
 	q[esp + i] = p[dathrb + i];
       }
       
       /* アプリ開始 */
-      start_app(0x1b, task->sel + 1000 * 8, esp, task->sel + 2000 * 8, &(task->tss.esp0));
+      start_app(0x1b, 0 * 8 + 4, esp, 1 * 8 + 4, &(task->tss.esp0));
 
       /* アプリ終了処理 */
       shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
