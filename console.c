5a24f8a78a0ab643090c1669acc81d4ee2d34baf
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-19 11:19:48 +0900

    コンソールをマウスで閉じる

--- console.c
+++ console.c
@@ -57,6 +57,9 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 		 cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
 	cons.cur_c = -1;
       }
+      if (i == 4) {
+	cmd_exit(&cons, fat);
+      }
       if (256 <= i && i <= 511) { /* キーボード・データ(タスクA経由) */
 	if (i == 8 + 256) {
 	  /* バックスペース */
